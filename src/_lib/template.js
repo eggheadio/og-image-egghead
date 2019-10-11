2ea34c015bd49285fd4cf005749b486e4746f7f9
evgeniy.nagalskiy <evgeniy.nagalskiy@gmail.com>
2019-10-11 22:50:19 +0300

    wip

--- src/_lib/template.js
+++ src/_lib/template.js
@@ -60,91 +60,22 @@ function InstructorGuide({ parsedReq }) {
           display: "flex",
           flexDirection: "column",
           fontFamily: "Helvetica, Arial, Sans-Serif",
-          padding: "40px",
-          height: "100%"
+          padding: "30px",
+          height: "100%",
+          backgroundSize: "contain"
         }}
       >
         <div
           css={{
             display: "flex",
             alignItems: "center",
-            flexShrink: 0
+            justifyContent: "center",
+            height: "100%",
+            fontSize: "86px",
+            paddingBottom: "65px"
           }}
         >
-          <img
-            css={{ height: "75px" }}
-            src="https://d2ddoduugvun08.cloudfront.net/items/1h0S2B3Q2F2x3l181j1D/eggo.svg?X-CloudApp-Visitor-Id=1972787"
-            alt=""
-          />
-          <h1
-            css={{
-              paddingLeft: "15px"
-            }}
-          >
-            egghead instructor guide
-          </h1>
-        </div>
-        <div
-          css={{
-            display: "flex",
-            alignItems: "center",
-            flexGrow: 1,
-            width: "100%"
-          }}
-        >
-          <div
-            css={{
-              width: "440px",
-              flexShrink: 0
-            }}
-          >
-            <img
-              css={{
-                display: "block",
-                width: "100%"
-              }}
-              src="https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/167/square_480/EGH_JSthis_1000.png"
-              alt=""
-            />
-          </div>
-          <div
-            css={{
-              flexGrow: 1,
-              marginLeft: "40px"
-            }}
-          >
-            <h2
-              css={{
-                fontSize: "64px",
-                fontWeight: 600,
-                lineHeight: 1.1,
-                margin: 0
-              }}
-            >
-              {text}
-            </h2>
-            <div
-              css={{ marginTop: "30px", display: "flex", alignItems: "center" }}
-            >
-              <div css={{ flexShrink: 0, marginRight: "24px" }}>
-                <img
-                  src="https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/110/square_128/mpk-oct-2018-500x500.jpg"
-                  alt=""
-                  css={{ borderRadius: "50%", width: "80px", flexShrink: 0 }}
-                />
-              </div>
-              <h3
-                css={{
-                  fontSize: "32px",
-                  fontWeight: 500,
-                  lineHeight: 1.1,
-                  margin: 0
-                }}
-              >
-                Marius Schulz
-              </h3>
-            </div>
-          </div>
+          {text}
         </div>
       </div>
     </React.Fragment>
