7119cec52bc33c8df48ace447911d7ab926aa470
Vojta Holik <vojta@egghead.io>
2019-10-16 23:37:30 +0200

    add background image query

--- src/_lib/template.js
+++ src/_lib/template.js
@@ -1,8 +1,9 @@
 /** @jsx jsx */
-import React from "react";
-import { renderToStaticMarkup } from "react-dom/server";
-import axios from "axios";
-import { jsx, css, Global } from "@emotion/core";
+import React from 'react'
+import {renderToStaticMarkup} from 'react-dom/server'
+import axios from 'axios'
+import {jsx, css, Global} from '@emotion/core'
+import {isEmpty} from 'lodash'
 
 const reset = css`
   *,
@@ -15,7 +16,7 @@ const reset = css`
     font-style: normal;
     padding: 0;
     margin: 0;
-    color: #21262f;
+    color: #242529;
   }
   html {
     text-rendering: optimizeLegibility;
@@ -39,77 +40,100 @@ const reset = css`
   h3 {
     font-size: 2rem;
   }
-`;
+`
 
-function App({ resource, parsedReq }) {
+function App({resource, parsedReq}) {
   return (
     <div>
       <h1>-----{parsedReq.text}</h1>
     </div>
-  );
+  )
 }
 
-function InstructorGuide({ parsedReq }) {
-  const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
-  console.log("PARSED REQUEST", parsedReq);
+function InstructorGuide({parsedReq}) {
+  const {
+    text,
+    theme,
+    md,
+    fontSize,
+    images,
+    background,
+    widths,
+    heights,
+    resourceType,
+  } = parsedReq
+  console.log('PARSED REQUEST', parsedReq)
   return (
     <React.Fragment>
       <Global styles={reset} />
       <div
         css={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontFamily: "Helvetica, Arial, Sans-Serif",
-          padding: "30px",
-          height: "100%",
-          backgroundImage:
-            "url('https://res.cloudinary.com/dg3gyk0gu/image/upload/v1571146625/og-image-assets/instructor-guide_2x.png')",
-          backgroundSize: "contain"
-        }}
-      >
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'flex-end',
+          fontFamily: 'Helvetica, Arial, Sans-Serif',
+          padding: '50px 80px',
+          height: '100%',
+          backgroundImage: `url(${
+            !isEmpty(background)
+              ? background
+              : 'https://res.cloudinary.com/dg3gyk0gu/image/upload/v1571146625/og-image-assets/instructor-guide_2x.png'
+          })`,
+          backgroundSize: 'contain',
+        }}>
         <div
           css={{
-            color: "white",
-            backgroundColor: "#FF2D55",
-            fontSize: "60px",
-            fontWeight: 600,
-            textAlign: "center",
-            lineHeight: 1.25,
-            padding: "16px 32px",
-            maxWidth: "80%"
-          }}
-        >
-          {text}
+            position: 'absolute',
+            left: 50,
+            top: 50,
+            display: 'flex',
+            alignItems: 'center',
+          }}>
+          <img src={images[0]} width="70px" />
+          <h3 css={{marginLeft: 20}}>{resourceType.replace('-', ' ')}</h3>
         </div>
+        <span
+          css={{
+            color: 'white',
+            backgroundColor: '#242529',
+            //backgroundColor: '#FF2D55',
+            boxShadow: '32px 0 0 #242529, -32px 0 0 #242529',
+            fontSize: '45px',
+            fontWeight: 600,
+            lineHeight: 2.25,
+            padding: '8px 0',
+            fontFamily: "'Lora', serif",
+          }}>
+          {text}
+        </span>
       </div>
     </React.Fragment>
-  );
+  )
 }
 
 export async function getHtml(parsedReq) {
-  let markup;
+  let markup
 
-  console.log(parsedReq);
+  console.log(parsedReq)
   switch (parsedReq.resourceType) {
-    case "instructor-guide":
-      markup = renderToStaticMarkup(<InstructorGuide parsedReq={parsedReq} />);
-      break;
-    case "series":
+    case 'instructor-guide':
+      markup = renderToStaticMarkup(<InstructorGuide parsedReq={parsedReq} />)
+      break
+    case 'series':
       const resource = await axios
         .get(
           `https://egghead.io/api/v1/${parsedReq.resourceType}/${parsedReq.text}`
         )
-        .then(({ data }) => data);
+        .then(({data}) => data)
       markup = renderToStaticMarkup(
         <App resource={resource} parsedReq={parsedReq} />
-      );
-      break;
+      )
+      break
     default:
       markup = renderToStaticMarkup(
         <App resource={resource} parsedReq={parsedReq} />
-      );
+      )
   }
-  console.log(markup);
-  return markup;
+  console.log(markup)
+  return markup
 }
