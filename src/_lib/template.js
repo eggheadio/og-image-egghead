9cda6c3f1fae904eec914bd4661771f623e48a68
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2020-11-21 11:08:39 +0200

    works

--- src/_lib/template.js
+++ src/_lib/template.js
@@ -399,132 +399,195 @@ function Lesson({lesson, parsedReq, palette}) {
   )
 }
 
-function Talk({lesson, parsedReq, palette}) {
-  const vibrant = `rgba(${palette.Vibrant._rgb.toString()}, 1)`
-  const darkVibrant = `rgba(${palette.DarkVibrant._rgb.toString()}, 1)`
+function Talk({talk, parsedReq}) {
   const {images} = parsedReq
+  const {thumb_nail, instructor: {full_name, avatar_256_url} } = talk
   return (
     <React.Fragment>
       <Global styles={reset} />
       <div
-        css={{
-          padding: 50,
-          display: 'flex',
-          alignItems: 'center',
-          width: '100%',
-          height: '100%',
-          borderTop: `25px solid ${vibrant}`,
-        }}
-      >
-        <div css={{marginRight: 50, width: '100%', maxWidth: 130}}>
-          <img
-            src={lesson.instructor.avatar_256_url}
-            css={{
-              borderRadius: '50%',
-              width: '100%',
-              maxWidth: 130,
-            }}
-          />{' '}
-        </div>
-        <div
-          // right column holder
           css={{
+            alignItems: 'center',
             display: 'flex',
-            flexDirection: 'column',
+            justifyContent: 'space-between',
+            width: '100%',
             height: '100%',
-            justifyContent: 'center',
           }}
         >
           <div
             css={{
-              marginBottom: 30,
+              width: '100%',
+              maxWidth: 460,
+              height: '100%',
               display: 'flex',
               alignItems: 'center',
-              fontSize: 36,
-              fontWeight: 600,
-            }}
-          >
-            <img src={images[0]} width="72px" css={{marginRight: 15}} />{' '}
-            egghead.io
-          </div>
-          <h1
-            // title
-            css={{
-              fontSize:
-                lesson.title.length > 30
-                  ? lesson.title.length > 45
-                    ? lesson.title.length > 55
-                      ? 52
-                      : 56
-                    : 62
-                  : 66,
-              lineHeight: 1.3,
-              padding: 0,
-              marginBottom: 30,
-            }}
-          >
-            {lesson.title.replace(`’`, `'`)}
-          </h1>
-          <div
-            // byline holder
-            css={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-start',
-              fontSize: 32,
-              flexWrap: 'wrap',
-              div: {
-                padding: '15px 0',
-              },
+              justifyContent: 'center',
+              flexDirection: 'column',
+              position: 'relative',
+              overflow: 'hidden',
+              backgroundColor: 'black',
             }}
           >
             <div
-              // instructor
-              css={{marginRight: 36, display: 'flex', alignItems: 'center'}}
+              css={{
+                transform: 'rotateZ(15deg) scale(1.35)',
+                transformOrigin: 'center center',
+                width: '100%',
+                height: '100%',
+                opacity: 0.9,
+              }}
             >
-              {emojify(lesson.instructor.full_name)}
+              <img src={thumb_nail} width="460" css={{objectFit: 'cover', height: '100%'}} />
             </div>
             <div
-              // duration
-              css={{display: 'flex', alignItems: 'center', marginRight: 36}}
+              css={{
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
             >
               <svg
-                css={{marginRight: 10}}
                 xmlns="http://www.w3.org/2000/svg"
-                width="36"
-                height="36"
-                viewBox="0 0 36 36"
+                width="95"
+                height="95"
+                viewBox="0 0 95 95"
               >
-                <path
-                  fill="#A1A7BC"
-                  d="M18,0 C8.0588745,0 0,8.0588745 0,18 C0,27.9411255 8.0588745,36 18,36 C27.9411255,36 36,27.9411255 36,18 C36,13.2261032 34.1035769,8.64773275 30.7279221,5.27207794 C27.3522672,1.89642313 22.7738968,0 18,0 Z M18,32.4 C10.0470996,32.4 3.6,25.9529004 3.6,18 C3.6,10.0470996 10.0470996,3.6 18,3.6 C25.9529004,3.6 32.4,10.0470996 32.4,18 C32.4,21.8191175 30.8828615,25.4818138 28.1823376,28.1823376 C25.4818138,30.8828615 21.8191175,32.4 18,32.4 Z M23.58,19.134 L19.8,16.956 L19.8,9 C19.8,8.00588745 18.9941125,7.2 18,7.2 C17.0058875,7.2 16.2,8.00588745 16.2,9 L16.2,18.216 C16.2106581,18.3400236 16.2410401,18.4615518 16.29,18.576 C16.3270691,18.682795 16.375338,18.7853664 16.434,18.882 C16.4832556,18.9843112 16.5436188,19.0808923 16.614,19.17 L16.902,19.404 L17.064,19.566 L21.744,22.266 C22.0183252,22.4214848 22.3286825,22.5021777 22.644,22.5000445 C23.4637073,22.505731 24.1837581,21.9569026 24.3954984,21.1649942 C24.6072386,20.3730858 24.2571828,19.5381326 23.544,19.134 L23.58,19.134 Z"
-                />
-              </svg>{' '}
-              {convertTime(lesson.duration)}
+                <g fill="none" fillRule="evenodd">
+                  <circle
+                    cx="47.32"
+                    cy="47.32"
+                    r="46.32"
+                    fill="#FFF"
+                    stroke="#051721"
+                    strokeWidth="2"
+                  />
+                  <path
+                    fill="#252526"
+                    fillRule="nonzero"
+                    d="M40.0400015,60.361356 C40.0400015,61.764363 40.9808178,62.2925569 42.1559215,61.5316904 L59.1811153,50.5080682 C60.3497057,49.7514191 60.356219,48.5288657 59.1811153,47.7679992 L42.1559215,36.7443771 C40.9873312,35.987728 40.0400015,36.5221136 40.0400015,37.9147114 L40.0400015,60.361356 Z"
+                  />
+                </g>
+              </svg>
             </div>
-            {lesson.free_forever && (
+          </div>
+          <div
+            css={{
+              display: 'flex',
+              flexDirection: 'column',
+              padding: 75,
+              width: '100%',
+            }}
+          >
+            <div
+              css={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <img src={images[0]} width="60px" />
+              <h2
+                css={{ fontSize: 34, marginLeft: 14, color: 'rgba(0,0,0,0.8)' }}
+              >
+                egghead.io
+              </h2>
+            </div>
+
+            <h1
+              css={{
+                fontWeight: 700,
+                padding: '56px 0',
+                color: 'rgba(0, 0, 0, 0.9)',
+                lineHeight: 1.2,
+                fontSize:
+                  talk.title.length > 30
+                    ? talk.title.length > 45
+                      ? talk.title.length > 55
+                        ? 48
+                        : 52
+                      : 56
+                    : 60,
+              }}
+            >
+              {emojify(talk.title)}
+            </h1>
+            <h3
+              css={{
+                width: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                margin: '0 auto',
+                textAlign: 'center',
+              }}
+            >
+              Talk by{' '}
+              <span css={{ display: 'flex', alignItems: 'center' }}>
+                {avatar_256_url && (
+                  <img
+                    src={avatar_256_url}
+                    css={{ borderRadius: '50%', margin: '0 16px' }}
+                    width="56"
+                  />
+                )}
+                {full_name}
+              </span>
+            </h3>
+            <div
+            // byline holder
+              css={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-start',
+                fontSize: 32,
+                flexWrap: 'wrap',
+                div: {
+                  padding: '15px 0',
+                },
+              }}
+            >
               <div
-                // community resource
-                css={{display: 'flex', alignItems: 'center'}}
+                // duration
+                css={{display: 'flex', alignItems: 'center', marginRight: 36}}
               >
                 <svg
                   css={{marginRight: 10}}
                   xmlns="http://www.w3.org/2000/svg"
-                  width="42"
-                  height="38"
-                  viewBox="0 0 42 38"
+                  width="36"
+                  height="36"
+                  viewBox="0 0 36 36"
                 >
-                  <g fill="#FFB430">
-                    <path d="M38.64,15.54 L32.34,15.54 C31.052,12.488 28,10.36 24.472,10.36 C19.768,10.36 15.932,14.168 15.932,18.844 C15.932,23.52 19.768,27.328 24.472,27.328 C28,27.328 31.024,25.2 32.312,22.176 L38.64,22.176 C40.488,22.176 42,20.692 42,18.844 C42,17.052 40.488,15.54 38.64,15.54 Z M24.472,22.148 C22.652,22.148 21.168,20.692 21.168,18.872 C21.168,17.052 22.652,15.596 24.472,15.596 C26.292,15.596 27.776,17.052 27.776,18.872 C27.776,20.692 26.292,22.148 24.472,22.148 Z" />
-                    <path d="M37.912,31.192 L32.984,26.656 C32.62,26.32 32.088,26.348 31.752,26.684 C29.596,28.728 26.6,29.932 23.324,29.652 C18.032,29.232 13.776,24.92 13.412,19.656 C12.964,13.384 17.976,8.12 24.22,8.12 C26.964,8.12 29.456,9.128 31.388,10.808 C31.724,11.116 32.256,11.116 32.592,10.808 L37.604,6.216 C37.8,6.02 37.828,5.712 37.632,5.516 C33.964,1.876 28.84,-0.28 23.212,0.028 C14.364,0.476 7.056,7.056 5.516,15.54 L3.36,15.54 C1.512,15.54 0,17.052 0,18.9 C0,20.748 1.512,22.26 3.36,22.26 L5.516,22.26 C7.112,31.08 14.868,37.8 24.22,37.8 C29.624,37.8 34.468,35.56 37.94,31.976 C38.136,31.752 38.136,31.416 37.912,31.192 Z" />
-                  </g>
+                  <path
+                    fill="#A1A7BC"
+                    d="M18,0 C8.0588745,0 0,8.0588745 0,18 C0,27.9411255 8.0588745,36 18,36 C27.9411255,36 36,27.9411255 36,18 C36,13.2261032 34.1035769,8.64773275 30.7279221,5.27207794 C27.3522672,1.89642313 22.7738968,0 18,0 Z M18,32.4 C10.0470996,32.4 3.6,25.9529004 3.6,18 C3.6,10.0470996 10.0470996,3.6 18,3.6 C25.9529004,3.6 32.4,10.0470996 32.4,18 C32.4,21.8191175 30.8828615,25.4818138 28.1823376,28.1823376 C25.4818138,30.8828615 21.8191175,32.4 18,32.4 Z M23.58,19.134 L19.8,16.956 L19.8,9 C19.8,8.00588745 18.9941125,7.2 18,7.2 C17.0058875,7.2 16.2,8.00588745 16.2,9 L16.2,18.216 C16.2106581,18.3400236 16.2410401,18.4615518 16.29,18.576 C16.3270691,18.682795 16.375338,18.7853664 16.434,18.882 C16.4832556,18.9843112 16.5436188,19.0808923 16.614,19.17 L16.902,19.404 L17.064,19.566 L21.744,22.266 C22.0183252,22.4214848 22.3286825,22.5021777 22.644,22.5000445 C23.4637073,22.505731 24.1837581,21.9569026 24.3954984,21.1649942 C24.6072386,20.3730858 24.2571828,19.5381326 23.544,19.134 L23.58,19.134 Z"
+                  />
                 </svg>{' '}
-                Community Resource
+                {convertTime(talk.duration)}
               </div>
-            )}
+              {talk.free_forever && (
+                <div
+                  // community resource
+                  css={{display: 'flex', alignItems: 'center'}}
+                >
+                  <svg
+                    css={{marginRight: 10}}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="42"
+                    height="38"
+                    viewBox="0 0 42 38"
+                  >
+                    <g fill="#FFB430">
+                      <path d="M38.64,15.54 L32.34,15.54 C31.052,12.488 28,10.36 24.472,10.36 C19.768,10.36 15.932,14.168 15.932,18.844 C15.932,23.52 19.768,27.328 24.472,27.328 C28,27.328 31.024,25.2 32.312,22.176 L38.64,22.176 C40.488,22.176 42,20.692 42,18.844 C42,17.052 40.488,15.54 38.64,15.54 Z M24.472,22.148 C22.652,22.148 21.168,20.692 21.168,18.872 C21.168,17.052 22.652,15.596 24.472,15.596 C26.292,15.596 27.776,17.052 27.776,18.872 C27.776,20.692 26.292,22.148 24.472,22.148 Z" />
+                      <path d="M37.912,31.192 L32.984,26.656 C32.62,26.32 32.088,26.348 31.752,26.684 C29.596,28.728 26.6,29.932 23.324,29.652 C18.032,29.232 13.776,24.92 13.412,19.656 C12.964,13.384 17.976,8.12 24.22,8.12 C26.964,8.12 29.456,9.128 31.388,10.808 C31.724,11.116 32.256,11.116 32.592,10.808 L37.604,6.216 C37.8,6.02 37.828,5.712 37.632,5.516 C33.964,1.876 28.84,-0.28 23.212,0.028 C14.364,0.476 7.056,7.056 5.516,15.54 L3.36,15.54 C1.512,15.54 0,17.052 0,18.9 C0,20.748 1.512,22.26 3.36,22.26 L5.516,22.26 C7.112,31.08 14.868,37.8 24.22,37.8 C29.624,37.8 34.468,35.56 37.94,31.976 C38.136,31.752 38.136,31.416 37.912,31.192 Z" />
+                    </g>
+                  </svg>{' '}
+                  Community Resource
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
     </React.Fragment>
   )
 }
@@ -1089,20 +1152,16 @@ export async function getHtml(parsedReq) {
       break
     case 'talk':
       const talk = await axios
-        .get(`https://egghead.io/api/v1/lessons/${parsedReq.text}`)
-        .then(({data}) => data)
-      const talkPalette = await Vibrant.from(talk.instructor.avatar_256_url)
-        .getPalette()
-        .then((palette) => palette)
-      markup = renderToStaticMarkup(
-        <Talk lesson={talk} palette={talkPalette} parsedReq={parsedReq} />,
-      )
+          .get(`https://egghead.io/api/v1/talks/${parsedReq.text}`)
+          .then(({ data }) => data)
+        markup = renderToStaticMarkup(
+          <Talk talk={talk} parsedReq={parsedReq} />,
+        )
       break
     default:
       markup = renderToStaticMarkup(
         <App resource={resource} parsedReq={parsedReq} />,
       )
   }
-  // console.log(markup)
   return markup
 }
